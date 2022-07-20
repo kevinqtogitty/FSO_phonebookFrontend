fc427d86fb38e672e58cbb469d9081b3c36a8e1c
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-07-20 07:28:26 +0200

    Initial commit

---
+++ src/PhonebookList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { UserState } from "react";
+
+import Persons from "./Persons";
+
+const PhonebookList = ({persons}) => {
+    const showPersons = persons.map(person => person)
+
+    return (
+    <ul>
+        {showPersons.map(person => <Persons key={person.id} persons={person.name} number={person.number}/>)}
+    </ul>
+    )
+}
+
+export default PhonebookList
