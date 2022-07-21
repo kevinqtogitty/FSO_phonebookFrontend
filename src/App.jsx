ad51fd2276f98c2557582c60de0183f2964f0a4e
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-07-21 16:33:28 +0200

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Form from './Form'
 import PhonebookList from './PhonebookList'
 import Search from './Search'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([    
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])  
+  const [persons, setPersons] = useState([])  
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(hook, [])
 
   return (
     <div>
